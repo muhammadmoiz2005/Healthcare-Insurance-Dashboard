3c0f967ac033b94cf7ede47678875a1a59669f28
Muhammad Moiz <163903833+muhammadmoiz2005@users.noreply.github.com>
2026-05-31 20:05:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import warnings
 import os
 import sys
